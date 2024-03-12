7fa1c9ea3b3e820ccc562455e66048305effa32c
duc minh <21ducminh@gmail.com>
2024-03-12 15:41:49 +0700

    init source

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View style={{paddingTop: 40}} className="bg-red-200 pt-20">
+      <Text className="text-3xl text-center">Home</Text>
+    </View>
+  )
+}
