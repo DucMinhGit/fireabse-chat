399fc0396cdf2bb8f00b2d2dd56736ff5c5443c5
duc minh <21ducminh@gmail.com>
2024-03-14 15:42:52 +0700

    show last message and time

--- components/ChatItem.js
+++ components/ChatItem.js
@@ -1,16 +1,55 @@
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import { Image } from "expo-image";
-import { blurhash } from "../utils/common";
+import { blurhash, formatDate, getRoomId } from "../utils/common";
+import { collection, doc, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebaseConfig";
 
-export default function ChatItem({ item, router, noBorder }) {
+export default function ChatItem({ item, router, noBorder, currentUser }) {
+  
+  const [lastMessage, setLastMessage] = useState(undefined);
+
+  useEffect(() => {
+    let roomId = getRoomId(currentUser?.userId, item?.userId);
+    const docRef = doc(db, "rooms", roomId);
+    const messagesRef = collection(docRef, "messages");
+    const q = query(messagesRef, orderBy('createdAt', 'desc'));
+
+    let unsub = onSnapshot(q, (snapshot) => {
+      let allMessages = snapshot.docs.map(doc=> {
+        return doc.data();
+      });
+      setLastMessage(allMessages[0] ? allMessages[0] : null);
+    });
+
+    return unsub;
+  }, []);
+  
   const openChatRoom = () => {
     router.push({ pathname: "/chatRoom", params: item });
   };
+
+  const renderTime = () => {
+    if (lastMessage) {
+      let date = lastMessage?.createdAt;
+      return formatDate(new Date(date?.seconds * 1000));
+    }
+  }
+
+  const renderLastMessage = () => {
+    if (typeof lastMessage === 'undefined') return 'Loading...';
+    if (lastMessage) {
+      if (currentUser?.userId === lastMessage?.userId) return "You: " + lastMessage?.text;
+      return lastMessage?.text;
+    } else {
+      return 'Say Hi';
+    }
+  }
+
   return (
     <View>
       <TouchableOpacity
@@ -40,13 +79,15 @@ export default function ChatItem({ item, router, noBorder }) {
             <Text
               style={{ fontSize: hp(1.6) }}
               className="font-medium text-neutral-500"
-            ></Text>
+            >
+              { renderTime() }
+            </Text>
           </View>
           <Text
             style={{ fontSize: hp(1.6) }}
             className="font-medium text-neutral-500"
           >
-            Last message
+            {renderLastMessage()}
           </Text>
         </View>
       </TouchableOpacity>
