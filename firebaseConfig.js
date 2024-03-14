711938e1cc0e0e3d9d864d9abc43f5a8728f22ca
duc minh <21ducminh@gmail.com>
2024-03-14 15:47:36 +0700

    hide key

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -8,12 +8,7 @@ import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore, collection } from 'firebase/firestore'
 const firebaseConfig = {
-  apiKey: "AIzaSyCGdauBJEr4uEIx_hCJYi_gI8Atljrm1II",
-  authDomain: "fir-chat-a8dfe.firebaseapp.com",
-  projectId: "fir-chat-a8dfe",
-  storageBucket: "fir-chat-a8dfe.appspot.com",
-  messagingSenderId: "545603515345",
-  appId: "1:545603515345:web:9702a0246955ec49e666f0"
+  
 };
 
 // Initialize Firebase
